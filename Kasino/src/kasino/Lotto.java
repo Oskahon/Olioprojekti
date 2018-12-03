85c9c556fa69432d5ac763e455ba06db265f30be
Oskahon <43564508+Oskahon@users.noreply.github.com>
2018-12-03 10:04:53 +0200

    Päivitetty versio

--- Kasino/src/kasino/Lotto.java
+++ Kasino/src/kasino/Lotto.java
@@ -6,7 +6,6 @@
 package kasino;
 
 import java.util.Arrays;
-import java.util.Scanner;
 import java.util.Random;
 import java.util.stream.IntStream;
 import javax.swing.JOptionPane;
@@ -23,7 +22,7 @@ public class Lotto {
     private int[] valitutnumerot = new int[7];
 
     // Arpoo lottorivin
-    public void arvoNumerot() {
+/*    public void arvoNumerot() {
         for (int i = 0; i < 7; i++) {
 
             int luku = (int) rand.nextInt(40) + 1;
@@ -38,32 +37,45 @@ public class Lotto {
 
         }
         Arrays.sort(arvotutnumerot);
-    }
-
+        }*/
     // Pyytää käyttäjältä numerot
     public void annaNumerot() {
         for (int i = 0; i < 7; i++) {
 
-            String numero = JOptionPane.showInputDialog(null, "Anna " + (i + 1) + ".numero:");
-            int luku = Integer.parseInt(numero);
+            boolean sisaltaa, validi = false;
+            int luku = 0;
 
-            boolean sisaltaa = IntStream.of(valitutnumerot).anyMatch(x -> x == luku);
+            // Varmistetaan, että annettu arvo on kokonaisluku
+            while (!validi) {
+                String numero = JOptionPane.showInputDialog(null, "Anna " + (i + 1) + ".numero:");
+                try {
+                    luku = Integer.parseInt(numero);
+                    validi = true;
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(null, "Et antanut kokonaislukua...");
+                }
+            }
 
-            if (luku <= 0 || luku > 40) {
+            int arvo;
+            arvo = luku;
+
+            sisaltaa = IntStream.of(valitutnumerot).anyMatch(x -> x == arvo);
+
+            if (arvo <= 0 || arvo > 40) {
                 i--;
                 JOptionPane.showMessageDialog(null, "Antamasi luku ei ollut väliltä 1 - 40. Anna uusi luku.");
             } else if (sisaltaa) {
                 i--;
                 JOptionPane.showMessageDialog(null, "Antamasi numero on jo annettu kertaalleen. Anna uusi luku.");
             } else {
-                valitutnumerot[i] = luku;
+                valitutnumerot[i] = arvo;
             }
 
         }
         Arrays.sort(valitutnumerot);
     }
 
-    // Hakee arvotut numerot
+// Hakee arvotut numerot
     public int[] haeArvotutNumerot() {
         return arvotutnumerot;
     }
@@ -75,29 +87,89 @@ public class Lotto {
 
     // Tarkistaa oikeiden numeroiden määrän
     public void tarkistaNumreot() {
-        int oikeatnumerot = 0;
+        int oikeatnumerot;
         String valnumerot = "", arvnumerot = "";
-        for (int i = 0;i < arvotutnumerot.length; i++) {
+        int kierrokset = 0, oikein3 = 0, oikein4 = 0, oikein5 = 0, oikein6 = 0, saldo = 100;
+
+        // Ajetaan niin kauan, että saadaan 7. oikein.
+        do {
+
+            // Nollaa arvottujen numeroiden taulukon, sekä oikeiden numeroiden määrän   
+            arvotutnumerot = null;
+            arvotutnumerot = new int[7];
+            oikeatnumerot = 0;
+            saldo--;
+            kierrokset++;
+
+            
+            // Arpoo numerot
+            for (int i = 0; i < 7; i++) {
+
+                int luku = (int) rand.nextInt(40) + 1;
+
+                boolean sisaltaa = IntStream.of(arvotutnumerot).anyMatch(x -> x == luku);
+
+                if (sisaltaa) {
+                    i--;
+                } else {
+                    arvotutnumerot[i] = luku;
+                }
+
+            }
+            Arrays.sort(arvotutnumerot);
+            
+            // Vertaa arvottuja numeroita valittuihin numeroihin
+            for (int i = 0; i < arvotutnumerot.length; i++) {
+                int luku = arvotutnumerot[i];
+                boolean sisaltaa = IntStream.of(valitutnumerot).anyMatch(x -> x == luku);
+                if (sisaltaa) {
+                    oikeatnumerot++;
+                }
+            }
+            
+            // Laskee eri voittojen määrän
+            switch (oikeatnumerot) {
+                case 4:
+                    oikein4++;
+                    break;
+                case 5:
+                    oikein5++;
+                    break;
+                case 6:
+                    oikein6++;
+                    break;
+                default:
+                    break;
+            }
+            
+            
+        } while (oikeatnumerot < 7);
+
+        // Muutetaan taulukot String muotoon
+        for (int i = 0; i < arvotutnumerot.length; i++) {
             if (i < 6) {
-            valnumerot += valitutnumerot[i]+", ";
-            arvnumerot += arvotutnumerot[i]+", ";
+                valnumerot += valitutnumerot[i] + ", ";
+                arvnumerot += arvotutnumerot[i] + ", ";
             } else {
                 valnumerot += valitutnumerot[i];
                 arvnumerot += arvotutnumerot[i];
             }
         }
-
-        for (int i = 0; i < arvotutnumerot.length; i++) {
-            int luku = arvotutnumerot[i];
-            boolean sisaltaa = IntStream.of(valitutnumerot).anyMatch(x -> x == luku);
-            if (sisaltaa) {
-                oikeatnumerot++;
-            }
-        }
         
-        JOptionPane.showMessageDialog(null, "Valitut numerot: \n"+valnumerot+
-                                            "\n\nArvotut numerot:\n"+arvnumerot+
-                                            "\n\nSait " + oikeatnumerot + " oikein!");
+        // Laskee voittojen tulot ja lisää saldoon
+        oikein3 *= 2;
+        oikein4 *= 10;
+        oikein5 *= 50;
+        oikein6 *= 10000;
+        saldo = saldo + oikein3 + oikein4 + oikein5 + oikein6 + 1500000;
+
+        
+ 
+        // Tulosviesti
+        JOptionPane.showMessageDialog(null, "Valitut numerot: \n" + valnumerot
+                + "\n\nArvotut numerot:\n" + arvnumerot
+                + "\n\nSait " + oikeatnumerot + " oikein! \nTähän meni: " + kierrokset + " kierrosta!"
+                        + "\n Lopullinen saldosi on: "+saldo+" euroa!");
 
     }
 
