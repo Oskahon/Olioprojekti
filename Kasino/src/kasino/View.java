1eceb239944eb32c691aa92c88869b611271121a
katjasei <43564742+katjasei@users.noreply.github.com>
2018-12-03 10:16:29 +0200

    Update View.java

--- Kasino/src/kasino/View.java
+++ Kasino/src/kasino/View.java
@@ -23,7 +23,7 @@ public class View {
         // JCheckBox checkbox = new JCheckBox("Tuplaus");
         ImageIcon icon = new ImageIcon("bliz.png"); // Kuva
 
-        Object[] options = {"Tietovisa", "Lotto", "Peli 3", "Peli 4", "Tietoa tekijöistä", "Sulje"};
+        Object[] options = {"Tietovisa", "Lotto", "Synttärit", "Peli 4", "Tietoa tekijöistä", "Sulje"};
         int x = JOptionPane.showOptionDialog(null, "\n Valitse peli tai toiminto\n\n",
                 "BLIZZARD GAMES INC.",
                 // JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
