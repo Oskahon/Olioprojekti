b0faa52c1580904dbfa9e28e73480ae98f0f3ccf
Oskahon <43564508+Oskahon@users.noreply.github.com>
2018-12-04 13:18:56 +0200

    normaaliloton lisäys

--- Kasino/src/kasino/Lotto.java
+++ Kasino/src/kasino/Lotto.java
@@ -19,8 +19,91 @@ public class Lotto {
 
     private int[] arvotutnumerot = new int[7];
 
-    // Arpoo numeroita niin kauan, että saadaan 7.oikein. Tämän jälkeen laskee lopullisen saldon.
+    // Tavallinen lotto
     public String lottoStart(int[] numerot) {
+
+        int[] valitutnumerot = numerot;
+        String valnumerot = "", arvnumerot = "", tulos;
+        int oikeatnumerot = 0, voittosumma = 0;
+
+        // Arpoo numerot
+        for (int i = 0; i < 7; i++) {
+
+            int luku = (int) rand.nextInt(40) + 1;
+
+            boolean sisaltaa = IntStream.of(arvotutnumerot).anyMatch(x -> x == luku);
+
+            if (sisaltaa) {
+                i--;
+            } else {
+                arvotutnumerot[i] = luku;
+            }
+
+        }
+        Arrays.sort(arvotutnumerot);
+
+        // Vertaa arvottuja numeroita valittuihin numeroihin
+        for (int i = 0; i < arvotutnumerot.length; i++) {
+            int luku = arvotutnumerot[i];
+            boolean sisaltaa = IntStream.of(valitutnumerot).anyMatch(x -> x == luku);
+            if (sisaltaa) {
+                oikeatnumerot++;
+            }
+        }
+
+        // Muutetaan valitutnumerot taulukko String muotoon
+        for (int i = 0; i < valitutnumerot.length; i++) {
+            if (i < 6) {
+                valnumerot += valitutnumerot[i] + ", ";
+                arvnumerot += arvotutnumerot[i] + ", ";
+            } else {
+                valnumerot += valitutnumerot[i];
+                arvnumerot += arvotutnumerot[i];
+            }
+        }
+
+        // Voitonjako
+        switch (oikeatnumerot) {
+            case 3:
+                voittosumma = 2;
+                break;
+            case 4:
+                voittosumma = 10;
+                break;
+            case 5:
+                voittosumma = 50;
+                break;
+            case 6:
+                voittosumma = 10000;
+                break;
+            case 7:
+                voittosumma = 2000000;
+                break;
+            default:
+                break;
+        }
+
+        // Tulosviesti
+        if (oikeatnumerot < 3) {
+            tulos = "Valitut numerot: \n" + valnumerot
+                    + "\n\nArvotut numerot: \n" + arvnumerot
+                    + "\n\nSait " + oikeatnumerot + " oikein! "
+                    + "\n\n Valitettavasti et voittanut mitään.";
+        } else {
+            tulos = "Valitut numerot: \n" + valnumerot
+                    + "\n\nArvotut numerot: \n" + arvnumerot
+                    + "\n\nSait " + oikeatnumerot + " oikein! "
+                    + "\n\nVoitit " + voittosumma + " euroa!";
+        }
+
+        return tulos;
+
+    }
+
+    
+    // 7 Oikein peli
+    // Arpoo numeroita niin kauan, että saadaan 7.oikein. Tämän jälkeen laskee lopullisen saldon.
+    public String lotto7Start(int[] numerot) {
 
         int[] valitutnumerot = numerot;
         String valnumerot = "";
@@ -58,9 +141,9 @@ public class Lotto {
                 boolean sisaltaa = IntStream.of(valitutnumerot).anyMatch(x -> x == luku);
                 if (sisaltaa) {
                     oikeatnumerot++;
-                } 
+                }
             }
-            
+
             switch (oikeatnumerot) {
                 case 4:
                     oikein4++;
